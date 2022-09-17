c90b65e5e8a803c221bcedfb0cfcbf772b0ceb07
Dan Hariton <danhariton22@gmail.com>
2022-09-17 13:33:54 +0300

    Functions for Queue

--- main.c
+++ main.c
@@ -1,5 +1,39 @@
 #include <stdio.h>
+#include <stdbool.h>
+ int A[10];
+ int front = -1;
+ int rear = -1;
+
+
+bool isEmpty(){
+   if (front == -1 && rear == -1){
+   return true;
+   }
+   else{
+      return false;
+   }
+}
+
+
+int Enqueue(int x){
+   if (rear == sizeof(A) - 1){
+   printf("Queue is full");
+   }
+   else if (isEmpty())
+   {
+      front = rear = 0;
+   }
+   else {
+      rear = rear + 1;
+   }
+   A[rear]= x;
+}
+
+
 int main() {
-   printf("Hello, World!");
-   return 0;
+ 
+ 
+
+
+
 }
